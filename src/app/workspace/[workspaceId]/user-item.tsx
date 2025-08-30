b53a927ac53d7525c2262e85d92a7d92df1973d0
hieuhoa-dev <hieuhoa.dev.cf@gmail.com>
2025-08-30 15:53:39 +0700

    Channels and Members
    Signed-off-by: hieuhoa-dev <hieuhoa.dev.cf@gmail.com>

---
+++ src/app/workspace/[workspaceId]/user-item.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import {cva, VariantProps} from "class-variance-authority";
+
+import {cn} from "@/lib/utils";
+import {Button} from "@/components/ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+
+import {useWorkspaceId} from "@/hooks/use-workspace-id";
+
+import {Id} from "../../../../convex/_generated/dataModel";
+
+const userItemVariants = cva(
+        "flex items-center justify-start gap-1.5 font-normal h-7 px-4 text-sm overflow-hidden",
+    {
+        variants: {
+            variant: {
+                default: "text-[#f9edffcc]",
+                active: "text-[#481349] bg-white/90 hover:bg-white/90",
+            },
+        },
+        defaultVariants: {
+            variant: "default"
+        }
+    },
+)
+
+interface Props {
+    id: Id<"members">;
+    label?: string;
+    image?: string;
+    variant?: VariantProps<typeof userItemVariants>["variant"];
+}
+
+export const UserItem = ({
+                             id, label = "Member", image, variant,
+                         }: Props) => {
+    const workspaceId = useWorkspaceId();
+    const avatarFallBack = label.charAt(0).toUpperCase();
+    return (
+        <Button
+            variant="transparent"
+            size="sm"
+            asChild
+            className={cn(userItemVariants({variant}))}
+        >
+            <Link href={`/workspace/${workspaceId}/channel/${id}`}>
+                <Avatar className="size-5 rounded-md mr-1">
+                    <AvatarImage className="rounded-md" src={image}/>
+                    <AvatarFallback className="rounded-md bg-sky-500 text-white text-xs">
+                        {avatarFallBack}
+                    </AvatarFallback>
+                </Avatar>
+                <span className="text-sm truncate">{label}</span>
+            </Link>
+        </Button>
+    )
+}
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,34 +1,88 @@
-import { useCurrentMember } from "@/features/members/use-current-member";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { AlertTriangle, Loader } from "lucide-react";
-import { WorkspaceHeader } from "./workspace-header";
+import {AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonalIcon} from "lucide-react";
+
+import {WorkspaceHeader} from "./workspace-header";
+import {SidebarItem} from "@/app/workspace/[workspaceId]/sidebar-item";
+import {WorkspaceSection} from "@/app/workspace/workspace-section";
+
+import {useWorkspaceId} from "@/hooks/use-workspace-id";
+import {useCurrentMember} from "@/features/members/api/use-current-member";
+import {useGetWorkspace} from "@/features/workspaces/api/use-get-workspace";
+import {useGetChannels} from "@/features/channels/api/use-get-channels";
+import {useGetMembers} from "@/features/members/api/use-get-member";
+import {UserItem} from "@/app/workspace/[workspaceId]/user-item";
+
 
 export const WorkspaceSidebar = () => {
-    const workspaceId  = useWorkspaceId();
-    const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
-    const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+    const workspaceId = useWorkspaceId();
+    const {data: member, isLoading: memberLoading} = useCurrentMember({workspaceId});
+    const {data: workspace, isLoading: workspaceLoading} = useGetWorkspace({id: workspaceId});
+    const {data: channels, isLoading: channelsLoading} = useGetChannels({workspaceId});
+    const {data: members, isLoading: membersLoading} = useGetMembers({workspaceId});
 
-    if(workspaceLoading || memberLoading) {
+    const isLoading = membersLoading && membersLoading;
+
+    if (workspaceLoading || memberLoading) {
         return (
-        <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
-            <Loader className="size-5 animate-spin text-white"/>
-        </div>
+            <div className="flex flex-col bg-[#5E2C5F] h-full items-center justify-center">
+                <Loader className="size-5 animate-spin text-white"/>
+            </div>
         );
-    }   
-    if( !workspace || !member) {
+    }
+    if (!workspace || !member) {
         return (
-        <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
-            <AlertTriangle className="size-5 text-white"/>
-            <p className="text-sm text-white">
-                workspace not found or you are not a member
-            </p>
-        </div>
+            <div className="flex flex-col gap-y-2 bg-[#5E2C5F] h-full items-center justify-center">
+                <AlertTriangle className="size-5 text-white"/>
+                <p className="text-sm text-white">
+                    workspace not found or you are not a member
+                </p>
+            </div>
         );
-    }   
-  return (
+    }
+    return (
         <div className="flex flex-col bg-[#5E2C5F] h-full">
-            <WorkspaceHeader  workspace={workspace} isAdmin={member.role === "admin"} />
+            <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"}/>
+            <div className="flex flex-col px-2 mt-3">
+                <SidebarItem
+                    label='Threads'
+                    icon={MessageSquareText}
+                    id="threads"
+                />
+                <SidebarItem
+                    label='Drafts & Sent'
+                    icon={SendHorizonalIcon}
+                    id="drafts"
+                />
+            </div>
+            <WorkspaceSection
+                label='Channels'
+                hint='New channel'
+                onNew={() => {
+                }}
+            >
+                {channels?.map((item) => (
+                    <SidebarItem
+                        key={item._id}
+                        icon={HashIcon}
+                        label={item.name}
+                        id={item._id}
+                    />
+                ))}
+            </WorkspaceSection>
+            <WorkspaceSection
+                label='Channels'
+                hint='New channel'
+                onNew={() => {
+                }}
+            >
+                {members?.map((item) => (
+                    <UserItem
+                        key={item._id}
+                        id={item._id}
+                        label={item.user.name}
+                        image={item.user.image}
+                    />
+                ))}
+            </WorkspaceSection>
         </div>
-  )
+    )
 };
