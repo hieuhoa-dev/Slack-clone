382eb2f88e10011f63c5e44a20b06119f23649fb
hieuhoa-dev <hieuhoa.dev.cf@gmail.com>
2025-09-08 16:20:13 +0700

    Message List
    Signed-off-by: hieuhoa-dev <hieuhoa.dev.cf@gmail.com>

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,37 +1,51 @@
 "use client"
 
-import {useGetChannel} from "@/features/channels/api/use-get-channel";
-import {useChannelId} from "@/hooks/use-channel-id";
 import {Loader, TriangleAlert} from "lucide-react";
+
 import {Header} from "./header";
+import {MessageList} from "@/components/message-list";
 import {ChatInput} from "@/app/workspace/[workspaceId]/channel/[channelId]/chat-input";
+
+import {useChannelId} from "@/hooks/use-channel-id";
+import {useGetChannel} from "@/features/channels/api/use-get-channel";
 import {useGetMessages} from "@/features/messages/api/use-get-messages";
+
 
 const ChannelIdPage = () => {
     const channelId = useChannelId();
 
-    const {results} = useGetMessages({channelId});
+    const {results, status, loadMore} = useGetMessages({channelId});
     const {data: channel, isLoading: ChannelLoading} = useGetChannel({id: channelId});
 
-    if (ChannelLoading) {
-        return (<div className="h-full flex-1 flex items-center justify-center ">
-            <Loader className="size- animate-spin text-muted-foreground"/>
-        </div>);
+    if (ChannelLoading || status === "LoadingFirstPage") {
+        return (
+            <div className="h-full flex-1 flex items-center justify-center ">
+                <Loader className="size-5 animate-spin text-muted-foreground"/>
+            </div>
+        );
     }
+
     if (!channel) {
-        return (<div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center ">
-            <TriangleAlert className="size-5 text-muted-foreground"/>
-            <span className="text-sm text-muted-foreground">
+        return (
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center ">
+                <TriangleAlert className="size-5 text-muted-foreground"/>
+                <span className="text-sm text-muted-foreground">
                 Channel not found or you are not a member
             </span>
-        </div>);
+            </div>
+        );
     }
     return (
         <div className="h-full flex flex-col">
             <Header title={channel.name}/>
-            <div className="flex-1">
-                {JSON.stringify(results, null, 2)}
-            </div>
+            <MessageList
+                channelName={channel.name}
+                channelCreationTime={channel._creationTime}
+                data={results}
+                loadMore={loadMore}
+                isLoadingMore={status === "LoadingMore"}
+                canLoadMore={status === "CanLoadMore"}
+            />
             <ChatInput placeholder={`Message # ${channel.name}`}/>
         </div>
     );
