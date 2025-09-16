72761f1f45c4256204e6eb91c43776dffca4b905
Shinobi234 <tdnqang2343@gmail.com>
2025-09-16 10:40:29 +0700

    Cosmetic-improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+                                    children,
+                                }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
